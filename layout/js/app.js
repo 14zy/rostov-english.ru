06f2748700ce1a144e39149517f8a36a7f7052cf
14zy <just14zy@gmail.com>
2016-01-17 04:24:25 +0300

    new engine for pilot

---
+++ layout/js/app.js
@@ -0,0 +1,43 @@
+currentSchool = schools[window.school];
+
+$("#header").load('layout/templates/_header.html');
+$("#footer").load('layout/templates/_footer.html');
+
+$("#sidebar").load('layout/templates/_sidebar.html', function() {
+  $("#imgLogo").attr("src", window.school + "/images/logo.png");
+  $("#txtYear").html(currentSchool.year);
+  $("#txtAddress1").html(currentSchool.contacts.adressess.address1);
+  $("#linkPhone").attr("href", "tel:" + currentSchool.contacts.phone.link);
+  $("#txtPhone").html(currentSchool.contacts.phone.txt);
+  $("#linkWebsite").attr("href", "http://" + currentSchool.contacts.website);
+  $("#txtWebsite").html(currentSchool.contacts.website);
+  $("#addReview").attr("href", "mailto:info@rostov-english.ru?subject=Отзыв%20на%20" + currentSchool.name + "&body=Ваше%20имя:%0AОтзыв:");
+
+  VK.Widgets.Group("vk_groups2", {
+    mode: 0,
+    width: "192",
+    height: "400",
+    color1: 'FFFFFF',
+    color2: '2B587A',
+    color3: '5B7FA6'
+  }, currentSchool.contacts.socialMedia.vkontakte);
+});
+
+$("#txtTitle").html(currentSchool.fullName);
+
+$(function() {
+  $('#slideshow').cycle({
+    fx: 'fade',
+    timeout: 6000,
+    pager: '#slideshow-nav',
+    pagerAnchorBuilder: function(idx, slide) {
+      // return sel string for existing anchor
+      return '#slideshow-nav li:eq(' + (idx) + ') a';
+    },
+    speed: 'slow'
+  });
+  $('#direct').click(function() {
+    $('#nav li:eq(2) a').triggerHandler('click');
+    return false;
+  });
+});
---
+++ layout/js/data.js
@@ -0,0 +1,31 @@
+schools = {
+  pilot: {
+    name: "Пилот",
+    fullName: 'Школа английского языка "Пилот"',
+    year: "1999 год",
+    contacts: {
+      city: "Ростов-на-Дону",
+      phone: {
+        txt: "221-47-46",
+        link: "+78632214746"
+      },
+      website: "www.pilot-school.ru",
+      socialMedia: {
+        vkontakte: "15274033",
+        instagram: "",
+        facebook: "lang.centre",
+        odnoklassniki: ""
+      },
+      adressess: {
+        address1: "Чехова, д.50",
+        address2: "",
+        address3: ""
+      }
+    },
+    reviews: {},
+    business: {
+      email: "",
+      phone: ""
+    }
+  }
+};
